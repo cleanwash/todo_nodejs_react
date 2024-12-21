f232ff16677a0fc2808541a4b19ca041abfa05c7
Jayone Lee <kenbugless@gmail.com>
2024-12-21 17:20:11 +0900

    feat:기본세팅 해주기

---
+++ be/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const mongoURI = `mongodb://localhost:27017/todo_app_react_node`;
+
+//useNewUrlParser: true는 예전 형식의 monogdb도 잘 읽을 수 있게 도와준다. 
+mongoose.connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
